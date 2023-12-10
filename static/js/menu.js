d0d5df27108c83d7c3091029c8df7916f1f0bd8a
sergiorobla <sergiorobla36@gmail.com>
2023-12-10 13:39:31 +0100

    modified:   index.html new file:   static/img/icons/angle-up.png modified:   static/js/menu.js modified:   static/js/menuRespon.js modified:   styles/style.css

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,13 +1,22 @@
-document.querySelector(".bars_menu").addEventListener("click", animateBars);
+const barsMenuButton = document.querySelector(".bars_menu");
+const line1Bars = document.querySelector(".line1_bars-menu");
+const line2Bars = document.querySelector(".line2_bars-menu");
+const line3Bars = document.querySelector(".line3_bars-menu");
+const containerMenu = document.querySelector(".menu");
 
-const line1_bars = document.querySelector(".line1_bars-menu");
-const line2_bars = document.querySelector(".line2_bars-menu");
-const line3_bars = document.querySelector(".line3_bars-menu");
-const container_menu = document.querySelector(".menu");
+barsMenuButton.addEventListener("click", animateBars);
 
-function animateBars(){
-  line1_bars.classList.toggle("activeline1_bars-menu");
-  line2_bars.classList.toggle("activeline2_bars-menu");
-  line3_bars.classList.toggle("activeline3_bars-menu");
-  container_menu.classList.toggle("activemenu");
+window.addEventListener("resize", function () {
+  const mobileWidthLimit = 625;
+
+  if (window.innerWidth > mobileWidthLimit && containerMenu.classList.contains("activemenu")) {
+    animateBars();
+  }
+});
+
+function animateBars() {
+  line1Bars.classList.toggle("activeline1_bars-menu");
+  line2Bars.classList.toggle("activeline2_bars-menu");
+  line3Bars.classList.toggle("activeline3_bars-menu");
+  containerMenu.classList.toggle("activemenu");
 }
--- static/js/menuRespon.js
+++ static/js/menuRespon.js
@@ -1,7 +1,12 @@
-document.querySelector(".menu-flags").addEventListener("click", animateMenu);
+document.querySelector(".menu-flags, .angle-down, .angle-up").addEventListener("click", animateMenu);
 
 const menuLan = document.querySelector(".flags");
+const angleDown = document.querySelector(".angle-down");
+const angleUp = document.querySelector(".angle-up");
+const menuFlags = document.querySelector(".menu-flags");
 
 function animateMenu(){
   menuLan.classList.toggle("activeflags");
-}
+  angleDown.classList.toggle("activeAngle-down");
+  angleUp.classList.toggle("activeAngle-up");
+};
