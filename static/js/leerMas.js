f2bc79cabe4ed943c6e823877238327659e3c3eb
sergiorobla <sergiorobla36@gmail.com>
2023-12-12 23:47:26 +0100

    C006

--- static/js/leerMas.js
+++ static/js/leerMas.js
@@ -1,12 +1,12 @@
 function alternarContenido() {
-    var contenido = document.getElementById("contenido");
+    var contenido = document.getElementById("contenido, contenido2, contenido3");
     var botonVerMas = document.getElementById("ver-mas");
 
     if (contenido.style.maxHeight) {
       contenido.style.maxHeight = null;
       botonVerMas.innerHTML = "Leer más...";
     } else {
-      contenido.style.maxHeight = "4500px";
+      contenido.style.maxHeight = "5000px";
       botonVerMas.innerHTML = "Leer menos...";
     }
   }
